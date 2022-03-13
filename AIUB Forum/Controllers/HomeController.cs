42ef80a9a12f28179190346c0bbfcabe184a84a3
NafisianCastle <nafis.cseaiub@gmail.com>
2022-03-13 21:00:44 +0600

    registration done

--- AIUB Forum/Controllers/HomeController.cs
+++ AIUB Forum/Controllers/HomeController.cs
@@ -2,49 +2,27 @@
 using AIUB_Forum.Models.Database;
 using System.Linq;
 using System.Web.Mvc;
-using System.Web.Security;
 
 namespace AIUB_Forum.Controllers
 {
-    // [Authorize]
     public class HomeController : Controller
     {
-        private readonly AIUB_ForumEntities2 _db = new AIUB_ForumEntities2();
-        [Authorize]
         public ActionResult Index()
         {
-            var posts = _db.Posts.Include(p => p.User);
-            return View(posts.ToList());
-        }
-        [HttpGet]
-        public ActionResult Login()
-        {
-            if (User.Identity.IsAuthenticated) return RedirectToAction("Index");
             return View();
         }
-        [AllowAnonymous]
-        [HttpPost]
-        public ActionResult Login(User user)
-        {
-            var entities = new AIUB_ForumEntities2();
-            var data = (from e in entities.Users
-                        where e.Password.Equals(user.Password) &&
-                              e.Email.Equals(user.Email)
-                        select e).FirstOrDefault();
-            if (data != null)
-            {
-                FormsAuthentication.SetAuthCookie("data.UserName", false);
-                Session["usertype"] = data.UserType;
-                Session["userid"] = data.UserId;
-                return RedirectToAction("Index");
-            }
 
-            TempData["msg"] = "Invalid Credentials";
-            return RedirectToAction("Login");
-        }
-        [HttpGet]
-        public ActionResult Register()
+        public ActionResult About()
         {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
             return View();
         }
         [HttpPost]
