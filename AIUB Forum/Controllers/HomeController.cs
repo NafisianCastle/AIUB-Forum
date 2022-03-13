bbc4b994b0275dfc9d161aa2a837f7014528298c
NafisianCastle <nafis.cseaiub@gmail.com>
2022-03-13 21:01:01 +0600

    change

--- AIUB Forum/Controllers/HomeController.cs
+++ AIUB Forum/Controllers/HomeController.cs
@@ -1,33 +1,61 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using AIUB_Forum.Models.Database;
 using System.Linq;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace AIUB_Forum.Controllers
 {
+    // [Authorize]
     public class HomeController : Controller
     {
+        private readonly AIUB_ForumEntities2 _db = new AIUB_ForumEntities2();
+        [Authorize]
         public ActionResult Index()
         {
+            var posts = _db.Posts.Include(p => p.User);
+            return View(posts.ToList());
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated) return RedirectToAction("Index");
             return View();
         }
-
-        public ActionResult About()
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(User user)
         {
-            ViewBag.Message = "Your application description page.";
+            var entities = new AIUB_ForumEntities2();
+            var data = (from e in entities.Users
+                        where e.Password.Equals(user.Password) &&
+                              e.Email.Equals(user.Email)
+                        select e).FirstOrDefault();
+            if (data != null)
+            {
+                FormsAuthentication.SetAuthCookie("data.UserName", false);
+                Session["usertype"] = data.UserType;
+                Session["userid"] = data.UserId;
+                return RedirectToAction("Index");
+            }
 
-            return View();
+            TempData["msg"] = "Invalid Credentials";
+            return RedirectToAction("Login");
         }
-
-        public ActionResult Contact()
+        [HttpGet]
+        public ActionResult Register()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
         [HttpPost]
         public ActionResult Register(User user)
         {
+            user.UserType = "User";
+            user.CreationDate= DateTime.Today;
+            if (!ModelState.IsValid) return View(user);
+            _db.Users.Add(user);
+            _db.SaveChanges();
             return RedirectToAction("Index");
         }
         [Authorize]
